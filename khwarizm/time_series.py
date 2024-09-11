51a2a6a09258ae5b77a81a08ff3c087bc5abe9b2
Ahmed Alghali <95593096+A7med7x7@users.noreply.github.com>
2024-09-11 09:36:49 +0000

    working with time features, adding rolling features function

--- khwarizm/time_series.py
+++ khwarizm/time_series.py
@@ -1,6 +1,56 @@
 import matplotlib.pyplot as plt
 from statsmodels.tsa.seasonal import seasonal_decompose
+import seaborn as sns
 import pandas as pd
+import numpy as np
+# adding rolling features with defined windows as attributes
+# time-series decomposition 
+class Features:
+
+    def __init__(self, dataset:pd.DataFrame,trainset: pd.DataFrame, testset: pd.DataFrame, date_feature: str):
+        self.dataset = dataset
+        self.trainset = trainset
+        self.testset = testset
+        self.date_feature = date_feature
+        
+    @staticmethod
+    def rolling(self, feature: str, window: int):
+        for dataset in (self.trainset, self.testset):
+            dataset[f"{feature}_rolling_max_{window}"] = dataset[feature].rolling(window).max()
+            return dataset
+    
+    @staticmethod
+    def time_features(self, dataset: pd.DataFrame) -> pd.DataFrame:
+        time_cols = ['date','Date','dates','timestamp','TimeStamp','dates']
+        if self.date_feature in time_cols:
+            if self.date_feature in dataset.columns:
+
+                dataset['date'] = pd.to_datetime(dataset[col])
+                dataset['Year'] = dataset['date'].dt.year
+                dataset['month'] = dataset['date'].dt.month
+                dataset['day'] = dataset['date'].dt.day
+                dataset['Weekday'] = dataset['date'].dt.weekday
+                dataset['Year_week'] = dataset['Year'].astype(str) + '-' + dataset['Weekday'].astype(str)
+                dataset['month_day'] = dataset['month'].astype(str) + '-' + dataset['day'].astype(str)
+                    
+        return dataset
+
+
+def time_features(dataset:pd.DataFrame) -> pd.DataFrame:
+    time_col = ['date','Date','dates','timestamp','TimeStamp','dates']
+    for col in time_col: 
+        if col in dataset.columns:
+            dataset['date'] = pd.to_datetime(dataset[col])
+            dataset['Year'] = dataset['date'].dt.year
+            dataset['month'] = dataset['date'].dt.month
+            dataset['day'] = dataset['date'].dt.day
+            dataset['Weekday'] = dataset['date'].dt.weekday
+            dataset['Year_week'] = dataset['Year'].astype(str) + '-' + dataset['Weekday'].astype(str)
+            dataset['month_day'] = dataset['month'].astype(str) + '-' + dataset['day'].astype(str)
+            
+    return dataset
+
+
 
 def plot_numerical_distributions(dataset:pd.DataFrame):
     numerical_cols = dataset.select_dtypes(include=['number']).columns
@@ -17,8 +67,8 @@ def plot_numerical_distributions(dataset:pd.DataFrame):
     plt.tight_layout()
     plt.show()
 
-def plot_target_curve(datase,time_feature: str,target: str): 
-    dataset[feature] = np.arange(len(dataset.index))
+def plot_target_curve(dataset,time_feature: str,target: str): 
+    dataset[target] = np.arange(len(dataset.index))
     plt.rc(
         "axes",
         labelweight="bold",
